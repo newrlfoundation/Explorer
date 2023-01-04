59ba1eee346bb273993d903f1077fe0a0b3a9fdb
Pradeep A <sharmapradeep872@gmail.com>
2023-01-04 18:10:53 +0530

    `block` and `block ID`page  logic

---
+++ app/block/[blockId]/page.tsx
@@ -0,0 +1,153 @@
+import moment from 'moment'
+import Link from 'next/link'
+import React from 'react'
+
+type PageProps = {
+    params: {
+        blockId: number
+    }
+}
+
+async function getBlockDetials(blockId: number) {
+    const res = await fetch(`https://mainnetgw.newrl.net/get-archived-blocks?start_index=${blockId}&end_index=${blockId}`, { cache: "force-cache" });
+    if (!res.ok) {
+        throw new Error('Failed to fetch data');
+    }
+    return res.json();
+}
+
+async function page({ params: { blockId } }: PageProps) {
+    const data = await getBlockDetials(blockId)
+    const block = data['blocks'][0]
+    const hash = data['hashes']
+    return (
+        <div className='p-10'>
+            <nav className="flex" aria-label="Breadcrumb">
+                <ol className="inline-flex items-center space-x-1 md:space-x-3">
+                    <li className="inline-flex items-center">
+                        <Link href={"/"} className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">
+                            <svg className="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path></svg>
+                            Home
+                        </Link>
+                    </li>
+                    <li className="flex items-center">
+                        <Link href={"/block"} className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">
+                            <svg className="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+                            Blocks
+                        </Link>
+                    </li>
+                    <li aria-current="page">
+                        <div className="flex items-center">
+                            <svg className="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+                            <span className="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400"># {blockId}</span>
+                        </div>
+                    </li>
+                </ol>
+            </nav>
+
+            <div className='p-5 my-10 bg-white rounded-md'>
+                <div className='flex justify-between'>
+                    <h1>Block Details</h1>
+                </div>
+                <hr />
+                <div className='grid grid-cols-1 gap-4 md:grid-cols-2 pt-10'>
+                    <div>
+                        Index : <strong>{block.index}</strong>
+                    </div>
+                    <div>
+                        Timestamp: {moment(block.timestamp).calendar()} <span className='ml-2 text-xs'>{moment(block.timestamp).fromNow()}</span>
+                    </div>
+                    <div className='truncate'>
+                        Hash : {hash}
+                    </div>
+                    <div>
+                        Proof : {block.proof}
+                    </div>
+                    <div>
+                        Status : {block.status}
+                    </div>
+                    <div>
+                        Creator Wallet : {block.creator_wallet}
+                    </div>
+                    <div>
+                        Expected Miner : {block.expected_miner}
+                    </div>
+                    <div className="md:col-span-2 md:py-5">
+                        Committee List <span className='text-xs align-super'>[ {block.committee ? block.committee.length : 0} ]</span>
+                        <div className='flex justify-center items-center'>
+                            <textarea id="message" rows={4} readOnly className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="committee list..." value={block.committee && block.committee.map((id: number, index: number) => (
+                                `${id}\t`
+                            ))}>
+                            </textarea>
+                        </div>
+                    </div>
+                    <div className='md:col-span-2'>
+                        Transactions <span className='text-xs align-super'>[ {block.text.transactions ? block.text.transactions.length : 0} ]</span>
+                        <div className='flex flex-col justify-center items-center bg-zinc-300-400 text-neutral-700'>
+                            {
+                                block.text.transactions && block.text.transactions.map((trans: any, index: number) => (
+                                    <>
+                                        <div className='p-5'>
+                                            <div className='grid grid-cols-1 gap-4 md:grid-cols-2 bg-slate-100 p-2 rounded-t-md' key={index}>
+                                                <div>
+                                                    Timestamp : {moment(trans.transaction.timestamp).calendar()} <span className='ml-2 text-xs'>{moment(trans.transaction.timestamp).fromNow()}</span>
+                                                </div>
+                                                <div className='truncate'>
+                                                    Trans code : {trans.transaction.trans_code}
+                                                </div>
+                                                <div>
+                                                    Type : {trans.transaction.type}
+                                                </div>
+                                                <div>
+                                                    Currency : {trans.transaction.currency}
+                                                </div>
+                                                <div>
+                                                    Fee : {trans.transaction.fee}
+                                                </div>
+                                                <div className='md:col-span-2'>
+                                                    Description : {trans.transaction.descr}
+                                                </div>
+                                                <div className='truncate'>
+                                                    Wallet Address : <strong>{trans.transaction.specific_data.wallet_address}</strong>
+                                                </div>
+                                                <div>
+                                                    Network Address : <strong>{trans.transaction.specific_data.network_address}</strong>
+                                                </div>
+                                                <div>
+                                                    Timestamp : {moment(trans.transaction.specific_data.broadcast_timestamp).calendar()} <span className='ml-2 text-xs'>{moment(trans.transaction.specific_data.broadcast_timestamp).fromNow()}</span>
+                                                </div>
+                                                <div>
+                                                    Software Version : {trans.transaction.specific_data.software_version}
+                                                </div>
+                                                <div>
+                                                    Last Block Index : <Link href={`/block/${trans.transaction.specific_data.last_block_index}`} className="text-blue-600 underline">{trans.transaction.specific_data.last_block_index}</Link>
+                                                </div>
+                                                <div>
+                                                    Is child Txn : {trans.transaction.is_child_txn.toString()}
+                                                </div>
+                                            </div>
+
+                                            <div className='bg-zinc-200 rounded-b-md p-2 grid grid-cols-1'>
+                                                Signatures
+                                                <div className='pt-2 px-3'>
+                                                    <div className='truncate'>
+                                                        wallet_address :  {trans.signatures[0].wallet_address}
+                                                    </div>
+                                                    <div className='truncate'>
+                                                        MSG sign :  {trans.signatures[0].msgsign}
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </>
+                                ))
+                            }
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div >
+    )
+}
+
+export default page
---
+++ app/block/page.tsx
@@ -0,0 +1,101 @@
+import Link from 'next/link'
+import moment from "moment"
+import React from 'react'
+
+async function getLastIndex() {
+    const res = await fetch('https://mainnetgw.newrl.net/get-last-block-index', { next: { revalidate: 10 } });
+    if (!res.ok) {
+        throw new Error('Failed to fetch data');
+    }
+    return res.json();
+}
+
+async function getFullBlocks(blockValue: number) {
+    const res = await fetch(`https://mainnetgw.newrl.net/get-archived-blocks?start_index=${blockValue - 25}&end_index=${blockValue}`, { next: { revalidate: 10 } });
+    if (!res.ok) {
+        throw new Error('Failed to fetch data');
+    }
+    return res.json();
+}
+
+function extractTransaction(blocks: any) {
+    let _transactions = [];
+    for (let block of blocks) {
+        const transaction_signs = block.text.transactions;
+
+        for (const tx_sign of transaction_signs) {
+            const tx = tx_sign.transaction;
+            const sign = tx_sign.signatures[0]
+
+            _transactions.push({
+                timestamp: tx.timestamp,
+                transactionCode: tx.trans_code,
+                type: tx.type,
+                signer: sign.wallet_address,
+                fee: `${parseInt(tx.fee)} ${tx.currency}`,
+            })
+        }
+    }
+
+    return _transactions
+}
+
+async function page() {
+    const lastBlock = await getLastIndex();
+    let { blocks } = await getFullBlocks(lastBlock);
+    blocks = blocks.reverse();
+    const transactions = extractTransaction(blocks);
+
+    return (
+        <>
+            <div className='p-10'>
+                <nav className="flex" aria-label="Breadcrumb">
+                    <ol className="inline-flex items-center space-x-1 md:space-x-3">
+                        <li className="inline-flex items-center">
+                            <Link href={"/"} className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">
+                                <svg className="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path></svg>
+                                Home
+                            </Link>
+                        </li>
+                        <li>
+                            <div className="flex items-center">
+                                <svg className="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+                                Blocks
+                            </div>
+                        </li>
+                    </ol>
+                </nav>
+            </div>
+            {/* All Blocks */}
+            <div className="bg-white p-5 mx-10 md:px-10 rounded-md h-full overflow-y-auto">
+                <p className="text-center font-semibold p-5">Latest Blocks</p>
+                {
+                    blocks.map((block: any, index: number) => (
+                        <Link key={index} href={`/block/${block.index}`}>
+                            <div key={index} className="flex py-3 space-x-3 cursor-pointer border-indigo-500 hover:border-r-2">
+                                <div>
+                                    <p className="bg-rose-100 w-10 h-10 rounded-md flex items-center justify-center">
+                                        BK
+                                    </p>
+                                </div>
+                                <div className="w-36">
+                                    <p className="font-semibold">{block.index}</p>
+                                    <p className="text-xs">{moment(block.timestamp).calendar()}</p>
+                                </div>
+                                <div className="flex-grow truncate">
+                                    <span className="text-xs">Validator</span>: {block.creator_wallet}
+                                </div>
+                                <div className="w-20 text-center">
+                                    <p className="font-semibold"> <span className="text-xs font-normal">Total</span> {block.text.transactions.length} <span className="text-xs font-normal">txn</span></p>
+                                    <p className="text-xs">{`${(JSON.stringify(block).length / 1024).toFixed(2)} kb`}</p>
+                                </div>
+                            </div>
+                        </Link>
+                    ))
+                }
+            </div>
+        </>
+    )
+}
+
+export default page
