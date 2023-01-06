78fc5030b8fa2821fd47e7d3516921d75b92acc4
Pradeep A <sharmapradeep872@gmail.com>
2023-01-06 20:13:48 +0530

    updated `Navbar`

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -26,61 +26,36 @@ function Navbar() {
                         <li>
                             <button id="mega-menu-full-cta-image-button" data-collapse-toggle="mega-menu-full-image-dropdown" className="flex justify-between items-center py-2 pr-4 pl-3 w-full font-medium text-gray-700 border-b border-gray-100 md:w-auto hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-gray-400 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Blockchain <svg className="ml-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd"></path></svg></button>
                         </li>
-                        <li>
-                            <a href="https://docs.newrl.net/" target={"_blank"} className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Resources</a>
-                        </li>
                     </ul>
                 </div>
             </div>
             <div id="mega-menu-full-image-dropdown" className="hidden z-10 mt-1 bg-white border-gray-200 shadow-sm border-y dark:bg-gray-800 dark:border-gray-600 absolute w-full transition duration-150 ease-in-out origin-top-left">
-                <div className="grid py-5 px-4 mx-auto max-w-screen-xl text-sm text-gray-500 dark:text-gray-400 md:grid-cols-3 md:px-6">
+                <div className="grid py-5 px-4 mx-auto max-w-screen-xl text-sm text-gray-500 dark:text-gray-400 md:grid-cols-2 md:px-6 text-center">
                     <ul className="hidden mb-4 space-y-4 md:mb-0 md:block" aria-labelledby="mega-menu-full-image-button">
                         <li>
-                            <a href="#" className="hover:underline hover:text-blue-600 dark:hover:text-blue-500">
-                                View Txn
-                            </a>
+                            <Link href="/transaction" className="hover:underline hover:text-blue-600 dark:hover:text-blue-500">
+                                View Transactions
+                            </Link>
                         </li>
                         <li>
-                            <Link href="block" className="hover:underline hover:text-blue-600 dark:hover:text-blue-500">
-                                View Block
+                            <Link href="/block" className="hover:underline hover:text-blue-600 dark:hover:text-blue-500">
+                                View Blocks
                             </Link>
                         </li>
                     </ul>
                     <ul className="mb-4 space-y-4 md:mb-0">
                         <li>
-                            <a href="#" className="hover:underline hover:text-blue-600 dark:hover:text-blue-500">
+                            <a href="https://newrl.net/press.html" target={"_blank"} className="hover:underline hover:text-blue-600 dark:hover:text-blue-500">
                                 Our Blog
                             </a>
                         </li>
                         <li>
-                            <a href="#" className="hover:underline hover:text-blue-600 dark:hover:text-blue-500">
-                                Terms & Conditions
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" className="hover:underline hover:text-blue-600 dark:hover:text-blue-500">
-                                License
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" className="hover:underline hover:text-blue-600 dark:hover:text-blue-500">
-                                Resources
-                            </a>
+                            <a href="https://docs.newrl.net/" target={"_blank"} className="hover:underline hover:text-blue-600 dark:hover:text-blue-500">Resources</a>
                         </li>
                     </ul>
-                    <a href="#" className="p-8 text-left bg-local bg-gray-500 bg-center bg-no-repeat bg-cover rounded-lg bg-blend-multiply hover:bg-blend-soft-light dark:hover:bg-blend-darken" style={{ backgroundImage: 'https://flowbite.com/docs/images/dashboard-overview.png' }}>
-                        <p className="mb-5 max-w-xl font-extrabold tracking-tight leading-tight text-white">Preview the new Flowbite dashboard navigation.</p>
-                        <button type="button" className="inline-flex items-center px-2.5 py-1.5 text-xs font-medium text-center text-white border border-white rounded-lg hover:bg-white hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-700">
-                            Get started
-                            <svg className="ml-1 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-                        </button>
-                    </a>
                 </div>
             </div>
         </nav>
-
-
-
     )
 }
 
